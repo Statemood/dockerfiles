73870339191dd370ca33b449df188179a21a268c
Lin Ru <Lin.Ru@msn.com>
2018-12-13 17:10:50 +0800

    add es 5.6.14, debug support

--- Dockerfiles/elasticsearch/5.6.14/run.sh
+++ Dockerfiles/elasticsearch/5.6.14/run.sh
@@ -51,6 +51,12 @@ then
     es_cmd="$ES_JAVA_OPTS $cmd $log $data $net $ES_OPTS"
     echo "Start $name with cmd: $es_cmd"
 
+    if [ -z "$K8S_ES_DEBUG" = "true" ]
+    then
+        echo "`date +'%F %T'`Waiting for debug, 600s"
+        sleep 600
+    fi
+
     sudo -u $user $es_cmd 
 else
     "$@"
